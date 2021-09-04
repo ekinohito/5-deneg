3149296eca1a240f3345e3380ad8756bee04c3aa
ekinohito <zhenyagur9@gmail.com>
2021-09-04 03:32:35 +0300

    minor input fix

--- components/Input.js
+++ components/Input.js
@@ -2,20 +2,34 @@ import React from 'react'
 
 export default function Input({margin, placeholder, params, setParams, index}) {
     console.log(setParams && index && "norm")
-    return <input
-        style={{
-        borderColor: "#022EED",
-        borderWidth: "2px",
-        borderStyle: "solid",
-        borderRadius: "5px",
-        backgroundColor: "#F8EDED",
+    return <label style={{
+        margin: margin || "0.1rem",
         flexBasis: "auto",
         flexGrow: 1,
-        padding: "0.5rem",
-        width: "100px",
-        margin: margin || "0.1rem"
-    }}
-        placeholder={placeholder}
-        value={params && index && params[index]}
-        onChange={setParams && index && ((e) => setParams({...params, index: e.currentTarget.value}))}/>
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch"
+    }}>
+        {placeholder}
+        <div style={{
+            display: "flex",
+
+        }}>
+            <input
+                style={{
+                    borderColor: "#042EE4",
+                    borderWidth: "2px",
+                    borderStyle: "solid",
+                    borderRadius: "5px",
+                    backgroundColor: "#F8EDED",
+                    padding: "0.5rem",
+                    width: "100px",
+                    flexGrow: 1
+                }}
+                placeholder={placeholder}
+                value={params && index && params[index]}
+                onChange={setParams && index && ((e) => setParams({...params, [index]: e.currentTarget.value}))}/>
+        </div>
+
+    </label>
 }
